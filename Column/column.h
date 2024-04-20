3c35fbe9d72582c17e4d4cd1ec81d6fb0568db46
LorisCacciottolo <loriscacciottolo@gmail.com>
2024-04-20 18:59:05 +0200

    create_column function

--- Column/column.h
+++ Column/column.h
@@ -1,7 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 typedef struct {
     char *title;
-    int *date;
-    int size;
+    int PhySize;
+    int LogSize;
+    int *data;
 }COLUMN;
-COLUMN *create_column(char *title);
+
+COLUMN *create_column(char *title) {
+    COLUMN *column = (COLUMN*) malloc(sizeof(COLUMN));
+    if (column == NULL) {
+        printf("Impossible to allocate memory for the column \n");
+        return NULL;
+    }
+    column->title = strdup(title);
+    column->data = NULL;
+    column->PhySize = 0;
+    column->LogSize = 0;
+
+    return column;
+
+}
+
+
+
 
--- main.c
+++ main.c
@@ -1,1 +1,10 @@
+#include "column.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define REALOC_SIZE 256
+
+int main() {
+    COLUMN *mycol = create_column("My column");
+}
