c692a6c0526d3a588242d4ac3baa81b7f97ba43c
LorisCacciottolo <loriscacciottolo@gmail.com>
2024-05-20 12:40:40 +0200

    debug

--- Cdataframe/cdataframe.c
+++ Cdataframe/cdataframe.c
@@ -1,4 +1,4 @@
-#include "CDataframe.h"
+#include "cdataframe.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- main.c
+++ main.c
@@ -1,2 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+
+#include "cdataframe.h"
+#include "Column.h"
 
